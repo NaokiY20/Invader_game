ed69a49fb5f3c451ea42b432e936032c9f0d8129
Saito-Saito-Saito <last20c.boy@gmail.com>
2020-04-10 09:19:52 +0900

    IO's main bullet control must be repaired

--- config.py
+++ config.py
@@ -7,40 +7,61 @@ import sys
 logging.basicConfig(level=logging.DEBUG,format='%(levelname)s - %(filename)s - L%(lineno)d - %(message)s')
 
 
-# SIDES
+### SIDES
 INVADER = 1
 FIGHTER = -1
 
 
-# LEVELS
+### LEVELS
+FGT = 0 # fighter
 BGN = 1 # bginner
 EAS = 2 # easy
 MED = 3 # medium
 HRD = 4 # hard
-EXP = 5 # expert
+EXP = 5  # expert
 
 
-# FOR DISPLAY INDEX
+### FIGHTER LIFE
+LIFE_MAX = 1
+
+
+### FOR DISPLAY INDEX
 HRZ = 0 # horizontal
 VRT = 1 # vertical
 
 
-# DISPLAYED SIZE
+### DISPLAYED SIZE
 SCR_SIZ = (400, 500)  # whole screen
 PAD_SIZ = 10    # padding
 SQR_SIZ = 8 # aircrafts are build four squares of siz * siz
-BUL_SIZ = 5
+BUL_SIZ = 5  # bullets
+BLC_SIZ = 3  # blocks of a guard
+FGT_AREA = (SQR_SIZ * 2) * 3    # the height of fighter's area
 
 
-# COLOURS (RGB max 255)
+### COLOURS (RGB max 255)
 BCG_COL = (0, 0, 0)  # background (black)
 FGT_COL = (0, 200, 0)  # fighter
 INV_COL = (200, 0, 0)  # invaders
 BUL_COL = (255, 255, 255)  # bullet (white)
+BLC_COL = (150, 150, 150)  # blocks
 
 
-# MOTION DISPLAY
+### MOTION DISPLAY
 # speed of aircrafts (classified by the level)
 FGT_SPEED = 1  # fighter's
 BUL_SPEED = 3
-BUL_WAIT = BUL_SIZ * 2  # interval during which next bullet is fired
+BUL_WAIT = BUL_SIZ * 10  # interval during which next bullet is fired; as a bullet is long, this interval gets longer
+
+
+### GUARDS
+GRD_SCTR = 4  # the number of guard 
+GRD_BLCS = 20  # each guard is composed of GRD_BLCS * GRD_BLCS square
+BLC_LEFT = 1  # block is left
+BLC_DSTR = 0  # block is destroyed (disappear)
+SID_MRGN = SQR_SIZ * 6  # margins width of both side of guards
+GRD_INTV = (SCR_SIZ[HRZ] - (PAD_SIZ * 2 + SID_MRGN * 2 + GRD_BLCS * BLC_SIZ * GRD_SCTR)) / (GRD_SCTR - 1)  # guard intetrval
+"""
+whole SCR_SIZ is filled as the follows
+    PAD_SIZ SID_MRGN SQR_SIZ*GRD_BLCS GRD_INTV SQR_SIZ*GRD_BLCS GRD_INTV ... SQR_SIZ*GRD_BLCS SID_MRGN PAD_SIZ
+"""
--- io.py
+++ io.py
@@ -9,7 +9,7 @@ import invader
 
 
 
-def aircraft_display(head_point, screen, side=INVADER):
+def aircraft_display(head_point: list, screen, side=INVADER):
     if side == INVADER:
         # head
         pygame.draw.rect(screen, INV_COL, Rect(head_point[0] - int(SQR_SIZ / 2), head_point[1] - int(SQR_SIZ / 2), SQR_SIZ, SQR_SIZ))
@@ -22,58 +22,97 @@ def aircraft_display(head_point, screen, side=INVADER):
         pygame.draw.rect(screen, FGT_COL, Rect(head_point[0] - int(SQR_SIZ / 2 * 3), head_point[1] + int(SQR_SIZ / 2), SQR_SIZ * 3, SQR_SIZ))
         
 
-def bullet_display(tip_point, screen):
-    pygame.draw.line(screen, BUL_COL, tip_point, (tip_point[HRZ], tip_point[VRT] + BUL_SIZ))
-    
+def bullet_display(tip_point: list, screen, side=INVADER):
+    if side == FIGHTER:
+        pygame.draw.line(screen, BUL_COL, tip_point, (tip_point[HRZ], tip_point[VRT] + BUL_SIZ))
+    elif side == INVADER:
+        pygame.draw.line(screen, BUL_COL, tip_point, (tip_point[HRZ], tip_point[VRT] - BUL_SIZ))
+    else:
+        logging.error('UNEXPECTED VALUE of SIDE in bullet_display')
+        sys.exit()
 
+
+def guard_display(screen):
+    for sector in range(GRD_SCTR):
+        for hrz in range(GRD_BLCS):
+            for vrt in range(GRD_BLCS):
+                if invader.Bullets[sector][hrz][vrt] == BLC_LEFT:
+                    pygame.draw.rect(screen, BLC_COL, Rect(PAD_SIZ + SID_MRGN + sector * (BLC_SIZ * GRD_BLCS + GRD_INTV) + BLC_SIZ * hrz, SCR_SIZ[VRT] - PAD_SIZ - FGT_AREA - (GRD_BLCS - vrt) * BLC_SIZ, BLC_SIZ, BLC_SIZ))
+                    
 
 def main():
     # building the screen
     pygame.init()
     screen = pygame.display.set_mode(SCR_SIZ)
     pygame.display.set_caption("Test")
-    bullet_wait = 0
+    fire_wait = 0
     
     while True:
         pressed_key = pygame.key.get_pressed()
-        bullet_wait = (bullet_wait + 1) % BUL_WAIT
+        fire_wait = (fire_wait + 1) % BUL_WAIT
         
+        
+        ### KEY EVENTS
         # firing bullets
-        if pressed_key[K_SPACE] and bullet_wait == 0:
+        if pressed_key[K_SPACE] and fire_wait == 0:
             bul = invader.Bullet(invader.Fighter.headpoint, FIGHTER)
             invader.Bullets.append(bul)
+            bul.tip_place[VRT] += BUL_SIZ
         # moving the fighter
-        if pressed_key[K_UP] and invader.Fighter.headpoint[VRT] > PAD_SIZ + SQR_SIZ * 2:
-            invader.Fighter.headpoint[VRT] -= FGT_SPEED
-        if pressed_key[K_DOWN] and invader.Fighter.headpoint[VRT] < SCR_SIZ[VRT] - PAD_SIZ - SQR_SIZ * 2:
-            invader.Fighter.headpoint[VRT] += FGT_SPEED
         if pressed_key[K_LEFT] and invader.Fighter.headpoint[HRZ] > PAD_SIZ + SQR_SIZ * 2:
             invader.Fighter.headpoint[HRZ] -= FGT_SPEED
         if pressed_key[K_RIGHT] and invader.Fighter.headpoint[HRZ] < SCR_SIZ[HRZ] - PAD_SIZ - SQR_SIZ * 2:
             invader.Fighter.headpoint[HRZ] += FGT_SPEED
+        
+        
+        ### PARAMETERS CONTROL
         # proceeding bullets
         for bul in invader.Bullets:
-            bul.tip_place[VRT] -= BUL_SPEED
-            # reaching the other side
-            if bul.tip_place[VRT] <= PAD_SIZ:
-                invader.Bullets.remove(bul)
-
-
-        # BACK GROUND
+            if bul.side == FIGHTER:
+                bul.tip_place[VRT] -= BUL_SPEED
+                # reaching the edge
+                if bul.tip_place[VRT] >= SCR_SIZ[VRT] - PAD_SIZ:
+                    invader.Bullets.remove(bul)
+                    continue    # going on the outer loop
+                # hitting an aircraft
+                for inv in invader.Invaders:
+                    if inv.headpoint[HRZ] - int(SQR_SIZ * 3 / 2) < bul.tip_place[HRZ] < inv.headpoint[HRZ] + int(SQR_SIZ * 3 / 2) and inv.headpoint[VRT] - int(SQR_SIZ * 3 / 2) <= bul.tip_place[VRT] < inv.headpoint[VRT] + int(SQR_SIZ / 2) - BUL_SIZ:
+                        invader.Bullets.remove(bul)
+                        inv.life -= 1
+                        break
+                else:
+                    continue    # going on the inner loop
+                continue    # going on the outer loop
+            elif bul.side == INVADER:
+                bul.tip_place[VRT] += BUL_SPEED
+                # reaching the edge
+                if bul.tip_place[VRT] <= PAD_SIZ:
+                    invader.Bullets.remove(bul)
+                    continue    # going on the outer loop
+                # hitting the fighter
+                elif invader.Fighter.headpoint[HRZ] - int(SQR_SIZ * 3 / 2) < bul.tip_place[HRZ] < invader.Fighter.headpoint[HRZ] + int(SQR_SIZ * 3 / 2) and invader.Fighter.headpoint[VRT] - int(SQR_SIZ / 2) <= bul.tip_place[VRT] < invader.Fighter.headpoint[VRT] + int(SQR_SIZ * 3 / 2) + BUL_SIZ:
+                    invader.Bullets.remove(bul)
+                    invader.Fighter.life -= 1
+                 
+                
+        ### DISPLAY
+        # background
         screen.fill(BCG_COL)
 
-
-        # AIRCRAFTS DISPLAY
+        # aircrafts
         for inv in invader.Invaders:
             aircraft_display(inv.headpoint, screen)
         aircraft_display(invader.Fighter.headpoint, screen, FIGHTER)
 
-        # BULLETS DISPLAY
+        # bullets
         for bul in invader.Bullets:
             bullet_display(bul.tip_place, screen)
-                    
 
-        # EVENTS
+        # guards
+        guard_display(screen)
+   
+
+        ### SPECIAL EVENTS
         for event in pygame.event.get():
             # close button
             if event.type == QUIT:
@@ -87,11 +126,11 @@ def main():
                     sys.exit()
                 
 
-        # screen upload
+        ### SCREEN UPLOAD
         pygame.display.update()
 
-        # loading time
-        pygame.time.wait(5)
+        ### LOADING TIME
+        pygame.time.wait(100)
 
 
 
